ed1fc77f178f6c7892c8830daae815df12c22087
MainDavis <dayamon12500@gmail.com>
2020-12-28 02:54:52 +0100

    Avanzado el Mapa.java

--- RPG/src/configuracion/Mapa.java
+++ RPG/src/configuracion/Mapa.java
@@ -1,16 +1,22 @@
 package configuracion;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Mapa extends Leer {
 	private int matriz[][];
 	private int objeto[];
 	private List<String> localizaciones = getLoc();
-	private List<String> AdyacenciasLocalizaciones = getAdLoc();
+	private List<String> AdyacenciasLocalizaciones;
 	private static final String nomLocalizacion = "";
 
 	public Mapa() {
-		matriz = new int[getNumLoc()][getNumLoc()];
+		this.AdyacenciasLocalizaciones = getAdLoc();
+		this.matriz = new int[getNumLoc()][getNumLoc()];
+		String[] strMatriz = (AdyacenciasLocalizaciones.get(1)).split(",");
+		System.out.println(strMatriz[0]);
+
+		System.out.println(Arrays.toString(strMatriz)); // [A, B, C, D]
 
 	}
 
