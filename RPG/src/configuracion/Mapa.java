5d8cc00a57f237f7824d2d4b36e36d26f1372d3d
MainDavis <dayamon12500@gmail.com>
2020-12-28 03:40:29 +0100

    Mapa.java terminada la matriz
    Terninada la matriz de adyacencias

--- RPG/src/configuracion/Mapa.java
+++ RPG/src/configuracion/Mapa.java
@@ -1,23 +1,38 @@
 package configuracion;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Mapa extends Leer {
 	private int matriz[][];
 	private int objeto[];
-	private List<String> localizaciones = getLoc();
+	private List<String> localizaciones;
 	private List<String> AdyacenciasLocalizaciones;
-	private static final String nomLocalizacion = "";
 
 	public Mapa() {
+		this.localizaciones = getLoc();
 		this.AdyacenciasLocalizaciones = getAdLoc();
-		this.matriz = new int[getNumLoc()][getNumLoc()];
-		String[] strMatriz = (AdyacenciasLocalizaciones.get(1)).split(",");
-		System.out.println(strMatriz[0]);
+		this.matriz = new int[getNumLoc()][getNumLoc()]; // Gracias a la matriz de adyacencias puedo usar el mapa como
+															// un grafo
 
-		System.out.println(Arrays.toString(strMatriz)); // [A, B, C, D]
+		// Preparamos los String por individual y los metemos en la matriz de
+		// adyacencias
+		for (int i = 0; i < getNumLoc(); i++) {
+			String[] strMatriz = (AdyacenciasLocalizaciones.get(i)).split(",");
+			if ((AdyacenciasLocalizaciones.get(i)).split(",").length > 1) {
+				strMatriz[0] = strMatriz[0].split("\\(")[1]; // Quito el primer nombre
+				strMatriz[strMatriz.length - 1] = strMatriz[strMatriz.length - 1].substring(0,
+						strMatriz[strMatriz.length - 1].length() - 1);
+				for (int j = 1; j < strMatriz.length; j++) {
+					strMatriz[j] = strMatriz[j].trim();
+				}
+			} else {
+				strMatriz[0] = strMatriz[0].split("\\(")[1];
+				strMatriz[0] = strMatriz[0].substring(0, strMatriz[0].length() - 1);
+			}
 
+			for (String element : strMatriz) {
+				matriz[i][localizaciones.indexOf(element)] = 1;
+			}
+		}
 	}
-
 }
