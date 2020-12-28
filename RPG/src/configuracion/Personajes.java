da0541d1b7824805515b74a708ad187f7bf9b937
ScorpionSud <ScorpionSud@users.noreply.github.com>
2020-12-28 02:53:41 +0100

    Personajes.java sin getters

--- RPG/src/configuracion/Personajes.java
+++ RPG/src/configuracion/Personajes.java
@@ -1,5 +1,20 @@
 package configuracion;
 
-public class Personajes {
+public final class Personajes extends Leer {
 
+	private String nombre;
+
+	private int objeto, creenciasPersonajes[], creenciasMapa[], registro[], localizacion;
+
+	public Personajes[] personaje = new Personajes[getNumPersonajes()];
+
+	public Personajes(String nombre, int objeto, int localizacion, int creenciasPersonajes[], int creenciasMapa[],
+			int registro[]) {
+		this.nombre = nombre;
+		this.objeto = objeto;
+		this.localizacion = localizacion;
+		this.creenciasPersonajes = creenciasPersonajes;
+		this.creenciasMapa = creenciasMapa;
+		this.registro = registro;
+	}
 }
