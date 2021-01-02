a0eacedcf4342ffc110ddf0d9087dd0e3c5985f4
MainDavis <dayamon12500@gmail.com>
2021-01-02 04:21:05 +0100

    El cambio de dios al mapa
    Bien.

--- RPG/src/configuracion/Mapa.java
+++ RPG/src/configuracion/Mapa.java
@@ -1,6 +1,5 @@
 package configuracion;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Mapa extends Leer {
@@ -17,36 +16,26 @@ public class Mapa extends Leer {
 		this.matriz = new int[getNumLoc()][getNumLoc()];
 		// Preparamos los String por individual y los metemos en la matriz de
 		// adyacencias
-		for (int i = 0; i < getNumLoc(); i++) {
-			String[] strMatriz = (adyacenciasLocalizaciones.get(i)).split(",");
-			if ((adyacenciasLocalizaciones.get(i)).split(",").length > 1) {
-				strMatriz[0] = strMatriz[0].split("\\(")[1]; // Quito el primer nombre
-				strMatriz[strMatriz.length - 1] = strMatriz[strMatriz.length - 1].substring(0,
-						strMatriz[strMatriz.length - 1].length() - 1);
-				for (int j = 1; j < strMatriz.length; j++) {
-					strMatriz[j] = strMatriz[j].trim();
-				}
-			} else {
-				strMatriz[0] = strMatriz[0].split("\\(")[1];
-				strMatriz[0] = strMatriz[0].substring(0, strMatriz[0].length() - 1);
-			}
 
-			for (String element : strMatriz) {
+		for (int i = 0; i < getNumLoc(); i++) {
+			String[] temp = adyacenciasLocalizaciones.get(i).split(",");
+
+			for (String element : temp) {
 				matriz[i][localizaciones.indexOf(element)] = 1;
 			}
 		}
 
 		// Preparo el array de listas
-		for (int i = 0; i < getObj().size(); i++) {
-			locObjeto[i] = new ArrayList<>();
-		}
-		// Meto los valores
-		for (String element : getLocObj()) {
-			String temp[] = element.split("\\(");
-			temp[1] = temp[1].substring(0, temp[1].length() - 1);
-			if (localizaciones.contains(temp[1])) {
-				locObjeto[localizaciones.indexOf(temp[1])].add(getObj().indexOf(temp[0]));
-			}
-		}
+//		for (int i = 0; i < getObj().size(); i++) {
+//			locObjeto[i] = new ArrayList<>();
+//		}
+//		// Meto los valores
+//		for (String element : getLocObj()) {
+//			String temp[] = element.split("\\(");
+//			temp[1] = temp[1].substring(0, temp[1].length() - 1);
+//			if (localizaciones.contains(temp[1])) {
+//				locObjeto[localizaciones.indexOf(temp[1])].add(getObj().indexOf(temp[0]));
+//			}
+//		}
 	}
 }
